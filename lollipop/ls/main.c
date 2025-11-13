d1bff023bc5e5bd4982365a971f43eadfd30aeb9
KusaReMKN <48670724+KusaReMKN@users.noreply.github.com>
2025-11-13 23:49:39 +0900

    Support -l option

--- lollipop/ls/main.c
+++ lollipop/ls/main.c
@@ -3,6 +3,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+#include <linux/if_tun.h>
+
 #include <err.h>
 #include <errno.h>
 #include <dirent.h>
@@ -10,21 +12,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
 
 #include "path.h"
+#include "ulid.h"
 
 static void usage(FILE *fp);
+static int detail_packet(int dir, const char *name);
 
 int
 main(int argc, char *argv[])
 {
 	struct dirent *d;
 	DIR *dirp;
-	int c, dir, i, match;
+	int c, dir, i, lflag, match;
 
-	while ((c = getopt(argc, argv, "h")) != -1)
+	lflag = 0;
+	while ((c = getopt(argc, argv, "lh")) != -1)
 		switch (c) {
+		case 'l':
+			lflag++;
+			break;
 		case 'h':
 			usage(stdout);
 			exit(0);
@@ -42,7 +51,10 @@ main(int argc, char *argv[])
 
 	while (errno = 0, (d = readdir(dirp)) != NULL)
 		if (d->d_name[0] != '.')
-			(void)puts(d->d_name);
+			if (lflag)
+				(void)detail_packet(dirfd(dirp), d->d_name);
+			else
+				(void)puts(d->d_name);
 	if (errno != 0)
 		err(1, "%s", WAITINGDIR);
 
@@ -51,8 +63,53 @@ main(int argc, char *argv[])
 	return 0;
 }
 
+static int
+detail_packet(int dir, const char *name)
+{
+	struct stat st;
+	struct tm tm;
+	struct ulid ul;
+	struct timeval tv;
+	struct tun_pi pi;
+	int error, fd;
+	char fmt[64], str[64];
+
+	fd = openat(dir, name, O_RDONLY);
+	if (fd == -1)
+		return -1;
+
+	if (read(fd, &pi, sizeof(pi)) == -1) {
+		error = errno;
+		(void)close(fd);
+		errno = error;
+		return -1;
+	}
+
+	if (fstat(fd, &st) == -1) {
+		error = errno;
+		(void)close(fd);
+		errno = error;
+		return -1;
+	}
+
+	(void)close(fd);
+
+	if (parse_ulid(&ul, strchr(name, '.')+1) == -1)
+		tv.tv_sec = tv.tv_usec = 0;
+	else
+		timestamp_ulid(&tv, &ul);
+	(void)localtime_r(&tv.tv_sec, &tm);
+	(void)strftime(fmt, sizeof(fmt), "%FT%T.%%03d%z", &tm);
+	(void)snprintf(str, sizeof(str), fmt, tv.tv_usec / 1000);
+
+	(void)printf("%04X\t%zu\t%s\t%s\n",
+			(unsigned)pi.proto, (size_t)st.st_size, str, name);
+
+	return 0;
+}
+
 static void
 usage(FILE *fp)
 {
-	(void)fprintf(fp, "usage: lollipop ls [-h]\n");
+	(void)fprintf(fp, "usage: lollipop ls [-lh]\n");
 }
